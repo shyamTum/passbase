6be5cbcb2a7153a3783b28331daf372d766a72f1
shyamTum <ga95san@mytum.de>
2019-08-17 23:41:46 +0200

    Added primary dashboard

---
+++ mock-dashboard/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Admin, Resource, ListGuesser } from 'react-admin';
+import jsonServerProvider from 'ra-data-json-server';
+import { UserList } from './users';
+
+const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
+//const App = () => <Admin dataProvider={dataProvider} />;
+const App = () => (
+   <Admin dataProvider={dataProvider}>
+        <Resource name="posts" list={ListGuesser} />
+        <Resource name="users" list={UserList} />
+  </Admin>
+);
+export default App;
