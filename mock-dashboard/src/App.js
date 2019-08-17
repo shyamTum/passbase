225f29993f7f4d4c1d6fa418684d8a94d6d06c52
shyamTum <ga95san@mytum.de>
2019-08-18 01:13:50 +0200

    Added functionality for Login

--- mock-dashboard/src/App.js
+++ mock-dashboard/src/App.js
@@ -2,11 +2,16 @@ import React from 'react';
 import { Admin, Resource, ListGuesser } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 import { UserList } from './users';
+import { Dashboard } from './Dashboard';
+import authProvider from './authProvider';
+import CustomLoginPage from './LoginPage';
+
+import MyLoginPage from './MyLoginPage';
+import MyLogoutButton from './MyLogoutButton';
 
 const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
-//const App = () => <Admin dataProvider={dataProvider} />;
 const App = () => (
-   <Admin dataProvider={dataProvider}>
+   <Admin dataProvider={dataProvider} authProvider={authProvider} LoginPage = {CustomLoginPage}>
         <Resource name="posts" list={ListGuesser} />
         <Resource name="users" list={UserList} />
   </Admin>
--- mock-dashboard/src/authProvider.js
+++ mock-dashboard/src/authProvider.js
@@ -3,8 +3,24 @@ import { AUTH_LOGIN, AUTH_LOGOUT, AUTH_ERROR, AUTH_CHECK } from 'react-admin';
 export default (type, params) => {
     // called when the user attempts to log in
     if (type === AUTH_LOGIN) {
-        const { username } = params;
-        localStorage.setItem('username', username);
+        const { username, password } = params;
+        //localStorage.setItem('username', username);
+        const request = new Request('https://mydomain.com/authenticate', {
+            method: 'POST',
+            body: JSON.stringify({ username, password }),
+            headers: new Headers({ 'Content-Type': 'application/json' }),
+        })
+        return fetch(request)
+            .then(response => {
+                if (response.status < 200 || response.status >= 300) {
+                    throw new Error(response.statusText);
+                }
+                return response.json();
+            })
+            .then(({ token }) => {
+                localStorage.setItem('token', token);
+            });
+    
         // accept all username/password combinations
         return Promise.resolve();
     }
